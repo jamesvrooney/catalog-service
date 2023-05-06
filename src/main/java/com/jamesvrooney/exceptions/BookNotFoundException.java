fdc3da019e43892d3e7287fec633a3b51d260503
jamesvrooney <jamesvrooney@hotmail.com>
2023-05-06 22:56:07 +0100

    Added tests.

---
+++ src/main/java/com/jamesvrooney/exceptions/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.jamesvrooney.exceptions;
+
+public class BookNotFoundException extends RuntimeException {
+
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+
+}
